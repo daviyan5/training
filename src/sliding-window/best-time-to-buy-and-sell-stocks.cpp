c20ca380ed3fdfcb37980a33e67c779e072efc41
Davi Yan <davi.yan_ma@yahoo.com>
2025-04-20 13:22:33 -0300

    best-time-to-buy-and-sell-stocks

---
+++ src/sliding-window/best-time-to-buy-and-sell-stocks.cpp
@@ -0,0 +1,60 @@
+/*
+Best Time to Buy and Sell Stock
+
+You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+Example 1:
+
+Input: prices = [10,1,5,6,7,1]
+
+Output: 6
+
+Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+Example 2:
+
+Input: prices = [10,8,7,5,2]
+
+Output: 0
+
+Explanation: No profitable transactions can be made, thus the max profit is 0.
+
+Constraints:
+
+    1 <= prices.length <= 100
+    0 <= prices[i] <= 100
+
+*/
+#include <vector>
+
+class Solution {
+    public:
+        int maxProfit(std::vector<int>& prices) {
+            size_t n = prices.size();
+            size_t start = 0, end = 1;
+
+            int profit = 0;
+            while (end < n) {
+                int max_sell = 0;
+                for (size_t i = end; i < n; ++i) {
+                    if (prices[i] > max_sell) {
+                        max_sell = prices[i];
+                        end = i;
+                    }
+                }
+                
+                for (size_t i = start; i < end; ++i) {
+                    if (prices[i] < prices[start]) {
+                        start = i;
+                    }
+                }
+                profit = std::max(max_sell - prices[start], profit);
+                end += 1;
+            }
+            return profit;
+        }
+};
